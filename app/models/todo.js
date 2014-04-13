7aee5f37d872301df222064d966240ee8237005f
Joshua Stoutenburg <jstoutenburg@deseretdigital.com>
2014-04-12 23:40:44 -0600

    Let's skip the fixtures step and build a real api

--- app/models/todo.js
+++ app/models/todo.js
@@ -3,22 +3,4 @@ var Todo = DS.Model.extend({
     isCompleted: DS.attr('boolean')
 });
 
-Todo.FIXTURES = [
-    {
-        id: 1,
-        title: 'Learn Ember.js',
-        isCompleted: true
-    },
-    {
-        id: 2,
-        title: '...',
-        isCompleted: false
-    },
-    {
-        id: 3,
-        title: 'Profit!',
-        isCompleted: false
-    }
-];
-
 export default Todo;
